7ae9adb1a612f9d170337575b9711418f32a1ed5
KaterinichViktor <107800265+KaterinichViktor@users.noreply.github.com>
2023-11-03 17:07:08 +0200

    x

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,4 +1,46 @@
+// import React, { useState } from 'react';
+
+// function Login() {
+//   const [formData, setFormData] = useState({
+//     email: '',
+//     password: '',
+//   });
+
+//   const handleChange = (e) => {
+//     const { name, value } = e.target;
+//     setFormData({ ...formData, [name]: value });
+//   };
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     // Додайте код для обробки логіна користувача з використанням formData
+//   };
+
+//   return (
+//     <div>
+//       <h2>Login</h2>
+//       <form onSubmit={handleSubmit}>
+//         <label>
+//           Email:
+//           <input type="email" name="email" value={formData.email} onChange={handleChange} />
+//         </label>
+//         <br />
+//         <label>
+//           Password:
+//           <input type="password" name="password" value={formData.password} onChange={handleChange} />
+//         </label>
+//         <br />
+//         <button type="submit">Login</button>
+//       </form>
+//     </div>
+//   );
+// }
+
+// export default Login;
+
+
 import React, { useState } from 'react';
+import { loginUserApi } from '../components/api'; // Імпорт функції для логіну
 
 function Login() {
   const [formData, setFormData] = useState({
@@ -6,14 +48,34 @@ function Login() {
     password: '',
   });
 
+  const [error, setError] = useState(null);
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    // Додайте код для обробки логіна користувача з використанням formData
+    
+    try {
+      // Виклик функції для логіну з використанням введених даних
+      const userData = await loginUserApi(formData);
+      
+      // Обробка успішного логіну (збереження токена, відображення інформації тощо)
+      console.log('Successful login:', userData);
+
+      // Очистка форми і помилок
+      setFormData({
+        email: '',
+        password: '',
+      });
+      setError(null);
+    } catch (error) {
+      // Обробка помилки при логіні
+      console.error('Login failed:', error);
+      setError('Login failed. Please check your credentials.');
+    }
   };
 
   return (
@@ -30,6 +92,7 @@ function Login() {
           <input type="password" name="password" value={formData.password} onChange={handleChange} />
         </label>
         <br />
+        {error && <p style={{ color: 'red' }}>{error}</p>}
         <button type="submit">Login</button>
       </form>
     </div>
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,4 +1,52 @@
+// import React, { useState } from 'react';
+
+// function Register() {
+//   const [formData, setFormData] = useState({
+//     name: '',
+//     email: '',
+//     password: '',
+//   });
+
+//   const handleChange = (e) => {
+//     const { name, value } = e.target;
+//     setFormData({ ...formData, [name]: value });
+//   };
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     // Додайте код для обробки реєстрації користувача з використанням formData
+//   };
+
+//   return (
+//     <div>
+//       <h2>Register</h2>
+//       <form onSubmit={handleSubmit}>
+//         <label>
+//           Name:
+//           <input type="text" name="name" value={formData.name} onChange={handleChange} />
+//         </label>
+//         <br />
+//         <label>
+//           Email:
+//           <input type="email" name="email" value={formData.email} onChange={handleChange} />
+//         </label>
+//         <br />
+//         <label>
+//           Password:
+//           <input type="password" name="password" value={formData.password} onChange={handleChange} />
+//         </label>
+//         <br />
+//         <button type="submit">Register</button>
+//       </form>
+//     </div>
+//   );
+// }
+
+// export default Register;
+
+
 import React, { useState } from 'react';
+import { registerUserApi } from '../components/api'; // Імпорт функції API для реєстрації
 
 function Register() {
   const [formData, setFormData] = useState({
@@ -14,7 +62,16 @@ function Register() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Додайте код для обробки реєстрації користувача з використанням formData
+    // Відправити дані реєстрації на сервер
+    registerUserApi(formData)
+      .then((response) => {
+        // Обробка успішної реєстрації
+        console.log('Користувач зареєстрований:', response);
+      })
+      .catch((error) => {
+        // Обробка помилки реєстрації
+        console.error('Помилка реєстрації:', error);
+      });
   };
 
   return (
@@ -43,3 +100,4 @@ function Register() {
 }
 
 export default Register;
+
